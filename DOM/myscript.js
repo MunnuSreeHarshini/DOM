dec65a1add1032da0771db7f11f4f05446ff3720
MunnuSreeHarshini <68006643+MunnuSreeHarshini@users.noreply.github.com>
2020-07-28 19:47:39 +0530

    Add files via upload
    DOM(Document Object Modal)
    Language translation

---
+++ DOM/myscript.js
@@ -0,0 +1,12 @@
+function francais()
+{
+  document.getElementById('p').innerHTML="John McCarthy (né le 3 septembre 1927, à Boston, dans le Massachusetts - mort le 24 octobre 20111) est le principal pionnier de l'intelligence artificielle avec Marvin Lee Minsky ; il incarne le courant mettant l'accent sur la logique symbolique.À la fin des années 1950, après un doctorat en mathématiques, il a créé avec Fernando Corbató la technique du temps partagé, qui permet à plusieurs utilisateurs d'employer simultanément un même ordinateur. Il est également le créateur du langage LISP, en 1958. Il reçoit le prix Turing en 1971 pour ses travaux en intelligence artificielle.";
+}
+function portugues()
+{
+  document.getElementById('p').innerHTML="John McCarthy (Boston, 4 de setembro de 1927 — Stanford, Califórnia, 23 de outubro de 2011[2]) foi um cientista da computação estadunidense.Conhecido pelos estudos no campo da inteligência artificial e por ser o criador da linguagem de programação Lisp. Recebeu o Prêmio Turing de 1972 e a Medalha Nacional de Ciências dos Estados Unidos de 1991.[3]";
+}
+function tamil()
+{
+  document.getElementById('p').innerHTML="ஜோன் மெக்கார்த்தி (John McCarthy,ஜோன் மெக்கார்த்தி (John McCarthy, செப்டம்பர் 4, 1927 - அக்டோபர் 24, 2011[1][2][3]) அமெரிக்க கணினி விஞ்ஞானியும் உணரறிவியல் அறிஞரும் ஆவார். செயற்கை நுண்ணறிவிற்காக தாம் ஆற்றிய பங்களிப்பிற்காக 1971 ஆம் ஆண்டு, டியூரிங் விருது வழங்கி கௌரவிக்கப்பட்டார். 1956 ஆம் ஆண்டு இடம்பெற்ற டார்த்மோத் மாநாட்டில் சமர்ப்பிக்கப்பட்ட தனது ஆய்வறிக்கையில் முதன் முதலாக செயற்கை நுண்ணறிவிற்கு பொருத்தமான ஆங்கிலப்பதமான Artificial Intelligence என்ற பதத்தை பயன்படுத்தி அறிமுகப்படுத்தினார். இவரே, லிஸ்ப் (Lisp) என்ற கணினி மொழியைக் கண்டுபிடித்தவராவார்.";
+}
